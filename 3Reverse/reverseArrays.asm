0e525199bf5b5cf6bc894a55564a3412d1a1214f
D-Guy Laptop <ddcool535@gmail.com>
2024-10-09 16:39:42 -0400

    Updated 3Reverse

--- 3Reverse/reverseArrays.asm
+++ 3Reverse/reverseArrays.asm
@@ -4,54 +4,54 @@
 PUBLIC _reverseArrays
 
 _reverseArrays PROC
-    mov rbx, rdi        
-    mov rdx, 4          
+    mov rbx, rdi            
+    mov rcx, 4              
 
 reverse_loop1:
-    cmp rdx, 0          
+    cmp rcx, 0              
     je reverse_done1
-    mov al, [rbx]       
-    push rax            
-    add rbx, 1          
-    dec rdx             
-    jmp reverse_loop1   
+    mov al, [rbx]          
+    push ax                 
+    add rbx, 1             
+    dec rcx                 
+    jmp reverse_loop1
 
 reverse_done1:
-    sub rbx, 4          
+    sub rbx, 4              
 
 reverse_loop2:
-    cmp rdx, 4          
+    cmp rcx, 4              
     je reverse_done2
-    pop rax             
-    mov [rbx], al       
-    add rbx, 1          
-    inc rdx             
-    jmp reverse_loop2   
+    pop ax                  
+    mov [rbx], al           
+    add rbx, 1              
+    inc rcx                
+    jmp reverse_loop2
 
 reverse_done2:
-    mov rbx, rsi        
-    mov rdx, 4          
+    mov rbx, rsi            
+    mov rcx, 4             
 
 reverse_loop3:
-    cmp rdx, 0          
+    cmp rcx, 0            
     je reverse_done3
-    mov eax, [rbx]      
-    push rax            
-    add rbx, 4         
-    dec rdx             
-    jmp reverse_loop3   
+    mov eax, [rbx]          
+    push rax               
+    add rbx, 4              
+    dec rcx                 
+    jmp reverse_loop3
 
 reverse_done3:
-    sub rbx, 16         
+    sub rbx, 16             
 
 reverse_loop4:
-    cmp rdx, 4          
+    cmp rcx, 4             
     je reverse_done4
-    pop rax             
-    mov [rbx], eax      
-    add rbx, 4          
-    inc rdx             
-    jmp reverse_loop4   
+    pop rax                 
+    mov [rbx], eax         
+    add rbx, 4              
+    inc rcx                 
+    jmp reverse_loop4
 
 reverse_done4:
     ret
