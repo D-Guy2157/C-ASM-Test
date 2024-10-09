fa7646ecbea3cba6a5d0eca0ccdb88767037cac0
D-Guy Desktop <ddcool535@gmail.com>
2024-10-08 21:43:30 -0400

    Updated 4Min/, still doesnt compile, new errors

--- 4Min/findMinIndex.asm
+++ 4Min/findMinIndex.asm
@@ -4,48 +4,49 @@
 PUBLIC _findMinIndex, _selectionSort
 
 _findMinIndex PROC
-    mov rbx, rdi
-    mov rcx, rsi
-    mov rdx, rdx
+    mov rbx, rdi            
+    mov ecx, esi           
+    mov edx, rdx            
 
 find_loop:
-    add rcx, 1
-    cmp rcx, rdx
-    jge end_min
-    mov eax, [rbx + rcx*4]
-    cmp eax, [rbx + rsi*4]
-    jl new_min
+    add ecx, 1             
+    cmp ecx, edx            
+    jge end_min            
+    mov eax, [rbx + ecx*4]  
+    cmp eax, [rbx + esi*4]  
+    jl new_min             
     jmp find_loop
 
 new_min:
-    mov esi, rcx
+    mov esi, ecx            
     jmp find_loop
 
 end_min:
-    mov eax, esi
+    mov eax, esi           
     ret
 _findMinIndex ENDP
 
 _selectionSort PROC
-    mov rbx, rdi
-    mov rdx, rsi
-    xor rdi, rdi
+    mov rbx, rdi            
+    mov edx, esi           
+    xor esi, esi            
 
 sort_loop:
-    cmp rdi, rdx
-    jge end_sort
-    mov rsi, rdi
-    call _findMinIndex
-    cmp rdi, eax
-    je next
-    mov ecx, [rbx + rdi*4]
-    mov edx, [rbx + eax*4]
-    mov [rbx + rdi*4], edx
-    mov [rbx + eax*4], ecx
+    cmp esi, edx            
+    jge end_sort            
+    mov ecx, esi            
+    call _findMinIndex      
+    cmp esi, eax            
+    je next                 
+
+    mov edx, [rbx + esi*4]  
+    mov ecx, [rbx + eax*4]
+    mov [rbx + esi*4], ecx
+    mov [rbx + eax*4], edx
 
 next:
-    add rdi, 1
-    jmp sort_loop
+    add esi, 1              
+    jmp sort_loop           
 
 end_sort:
     ret
